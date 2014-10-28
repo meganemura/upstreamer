574c3147f46881dbe2f76a5dd964d0a693e43422
meganemura <mura2megane@gmail.com>
2014-10-29 00:23:10 +0900

    Add `CLI#log_error` to put error message

--- lib/upstreamer/cli.rb
+++ lib/upstreamer/cli.rb
@@ -18,20 +18,20 @@ module Upstreamer
       repo = Rugged::Repository.new(directory)
 
       if repo.remotes['upstream']
-        STDERR.puts "Error: Remote 'upstream' already exists. (#{repo.remotes['upstream'].url})"
+        log_error "Error: Remote 'upstream' already exists. (#{repo.remotes['upstream'].url})"
         return
       end
 
       remote = repo.remotes['origin']                               # https://github.com/meganemura/bundler.git
       unless remote
-        STDERR.puts "Error: Could not find remote 'origin'. (#{directory})"
+        log_error "Error: Could not find remote 'origin'. (#{directory})"
         return
       end
       username_repository = remote.url[/github.com\/(.*)\.git/, 1]  # meganemura/bundler
 
       repository = Octokit.repository(username_repository)
       unless repository.fork?
-        STDERR.puts 'Error: this repository is not forked repository'
+        log_error "Error: This repository is not forked repository. (#{directory})"
         return
       end
       upstream_url = repository.parent.clone_url
@@ -46,6 +46,14 @@ module Upstreamer
 
     def current_directory
       Dir.pwd
+    end
+
+    def log_error(message)
+      STDERR.puts(message) unless quiet
+    end
+
+    def quiet
+      false
     end
   end
 end
